76fdd856956de266166ef401337d7e8ad2afd701
Sam Vitare <sandhya.palanki@gmail.com>
2020-10-29 20:30:38 -0500

    would you rather with smaller components 3 questions

---
+++ wyr2/would_you_rather_2/src/App.vue
@@ -0,0 +1,55 @@
+<!-- this is the parent -->
+
+<template>
+    <div id="app">
+        <h1>Would you rather...</h1>
+
+        <!-- if you want to see components on the webpage, reference them here -->
+        <would-you-rather v-bind:question="wyrQuestion"
+                          v-bind:answer1="wyrAnswer1"
+                          v-bind:answer2="wyrAnswer2"
+                          v-on:answer-changed="answerChanged">
+        </would-you-rather>
+
+        <p>{{ userSelectionMessage }}</p>
+    </div>
+</template>
+
+<script>
+    import WouldYouRather from './components/WouldYouRather.vue'
+
+    export default {
+        name: 'App',
+        components: {
+            WouldYouRather
+        },
+        data() {
+            return {
+                wyrQuestion: 'Would you rather be able to read minds or see one day into the future?',
+                wyrAnswer1: 'Read minds',
+                wyrAnswer2: 'See one day into the future',
+                userSelectionMessage: ''
+            }
+        },
+        methods: {
+            answerChanged(choice) {
+                this.userSelectionMessage = `Thank you! You chose, "${choice}".`
+            }
+        }
+    }
+</script>
+
+<style>
+    body { background: teal; }
+
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: yellowgreen;
+        margin-top: 60px;
+        background-color: #2c3e50;
+        padding: 15px;
+    }
+</style>
---
+++ wyr2/would_you_rather_2/src/components/WouldYouRather.vue
@@ -0,0 +1,65 @@
+<!-- this is the child component of the App.vue -->
+
+<template>
+    <div class="wyr">
+        <h2>Please make your choice!</h2>
+        <h3>{{ question }}</h3>
+
+        <!-- creating radio buttons for the answers -->
+        <input type="radio"
+               v-model="choice"
+               v-bind:value="answer1"
+               v-on:change="choiceMade">
+        <label>{{ answer1 }}</label>
+
+        <input type="radio"
+               v-model="choice"
+               v-bind:value="answer2"
+               v-on:change="choiceMade">
+        <label>{{ answer2 }}</label>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'WouldYouRather',
+        // never modify a prop in a component
+        props: {
+            question: String,
+            answer1: String,
+            answer2: String,
+        },
+        // collect this data & this data is sent to the parent App.vue
+        data() {
+            return {
+                choice: ''          // initially no choice made
+            }
+        },
+        methods: {
+            choiceMade() {
+                // send an event to the parent App.vue about the choice made
+                this.$emit('answer-changed', this.choice)
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h3
+    {
+        margin: 40px 0 0;
+        padding-bottom: 15px;
+    }
+
+    .wyr
+    {
+        border: 2px yellow solid;
+        background: cadetblue;
+        color: yellow;
+        margin: 20px;
+        padding: 15px;
+    }
+
+    label { padding-right: 15px; }
+</style>
